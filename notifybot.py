a1e5424e8b6caa41d677a0af442b1efdc3b2d3e9
Ruzan <ruzzanv@gmail.com>
2022-11-27 18:54:48 +0400

    update notifybot2

--- notifybot.py
+++ notifybot.py
@@ -3,7 +3,17 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-TOKEN = '5894373779:AAEO4Dd734mfoIP0BY-hBLOuW6Dp7ewANVo'
+TOKEN = $TOKEN_BOT_NOTIFY
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    user_full_name = message.from_user.full_name
+    logging.info(f'{user_id=} {user_full_name=}, {time.asctime()}')
+    await message.reply(f'Привет, {user_full_name}! Ты запустил бота помощника по управлению твоими подписками!')
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
