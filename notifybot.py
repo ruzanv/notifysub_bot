c41c907df5b954027425ea26aee0a97b2c3d413c
Ruzan <ruzzanv@gmail.com>
2022-11-27 17:28:47 +0400

    added venv

---
+++ notifybot.py
@@ -0,0 +1,9 @@
+import time
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = '5894373779:AAEO4Dd734mfoIP0BY-hBLOuW6Dp7ewANVo'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
